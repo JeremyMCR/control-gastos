657ee8ef4ffc99035bee2333db6f5589274c3334
JeremyMCR <97810428+JeremyMCR@users.noreply.github.com>
2023-04-28 22:25:03 -0600

    Cambios generales

--- src/Components/Gasto.jsx
+++ src/Components/Gasto.jsx
@@ -61,15 +61,20 @@ const Gasto = ({gasto, setGastoEditar, eliminarGasto}) => {
                     alt='Icono gasto'
                 />
                 <div className='descripcion-gasto'>
+                  
                     <p className='categoria'> {categoria} </p>
                     <p className='nombre-gasto'> {nombre} </p>
                     <p className='fecha-gasto'>
                         Agregado el: {''}
                         <span>{formatearFecha(fecha)}</span>
                     </p>
+                    
+                    <p className='cantidad-gasto'> 
+                        Cantidad
+                        <span> ₡{cantidad}</span>
+                    </p>
                 </div>
             </div>
-              <p className='cantidad-gasto'>₡{cantidad}</p>
         </div>
       </SwipeableListItem>
     </SwipeableList>
